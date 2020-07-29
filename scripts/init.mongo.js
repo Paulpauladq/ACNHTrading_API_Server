de2b13f0bbc0a5011ee50cedfd158ef90a76520c
Shengfu Zhang <zhang.aptx4869@gmail.com>
2020-07-28 23:25:59 -0700

    adopt camalCase to address ESLint error

--- scripts/init.mongo.js
+++ scripts/init.mongo.js
@@ -22,60 +22,60 @@ const listingsDB = [
   {
     id: 1,
     status: 'New',
-    seller_id: 1,
-    product_id: 'EpywQXABBcv2dipsP',
-    product_count: 1,
+    sellerId: 1,
+    productId: 'EpywQXABBcv2dipsP',
+    productCount: 1,
     created: new Date('2020-07-13'),
     expired: undefined,
     note: 'Don\'t send me empty offers.',
-    price_list: [
+    priceList: [
       {
-        product_id: "bell",
-        product_count: 1000000,
+        productId: 'bell',
+        productCount: 1000000,
       },
       {
-        product_id: "FAn9xRZvpBRAad3E6",
-        product_count: 20,
+        productId: 'FAn9xRZvpBRAad3E6',
+        productCount: 20,
       },
       {
-        product_id: "J4inuBziPCSGZNEPM",
-        product_count: 2,
+        productId: 'J4inuBziPCSGZNEPM',
+        productCount: 2,
       },
     ],
   },
   {
     id: 2,
     status: 'Pending',
-    seller_id: 2,
-    product_id: "XAnaYsJ8wouWyBBsu",
-    product_count: 3,
+    sellerId: 2,
+    productId: 'XAnaYsJ8wouWyBBsu',
+    productCount: 3,
     created: new Date('2020-07-15'),
     expired: undefined,
     note: 'Yohooooooooo',
-    price_list: [
+    priceList: [
       {
-        product_id: "FAn9xRZvpBRAad3E6",
-        product_count: 20,
+        productId: 'FAn9xRZvpBRAad3E6',
+        productCount: 20,
       },
       {
-        product_id: "t2LzAKDofAZYyqZ6r",
-        product_count: 2,
+        productId: 't2LzAKDofAZYyqZ6r',
+        productCount: 2,
       },
     ],
   },
   {
     id: 3,
     status: 'Closed',
-    seller_id: 1,
-    product_id: "54WzSWz3bv4j4279w",
-    product_count: 1,
+    sellerId: 1,
+    productId: '54WzSWz3bv4j4279w',
+    productCount: 1,
     created: new Date('2020-07-10'),
     expired: undefined,
     note: 'Accepting touch trade',
-    price_list: [
+    priceList: [
       {
-        product_id: "6wyLNus8QqXkbBu72",
-        product_count: 1,
+        productId: '6wyLNus8QqXkbBu72',
+        productCount: 1,
       },
     ],
   },
@@ -85,29 +85,29 @@ const offersDB = [
   {
     id: 1,
     status: 'New',
-    listing_id: 2,
-    seller_id: 2,
-    buyer_id: 1,
-    product_id: "FAn9xRZvpBRAad3E6",
-    product_count: 20,
+    listingId: 2,
+    sellerId: 2,
+    buyerId: 1,
+    productId: 'FAn9xRZvpBRAad3E6',
+    productCount: 20,
   },
   {
     id: 2,
     status: 'Rejected',
-    listing_id: 3,
-    seller_id: 1,
-    buyer_id: 2,
-    product_id: "6wyLNus8QqXkbBu72",
-    product_count: 1,
+    listingId: 3,
+    sellerId: 1,
+    buyerId: 2,
+    productId: '6wyLNus8QqXkbBu72',
+    productCount: 1,
   },
   {
     id: 3,
     status: 'Accepted',
-    listing_id: 1,
-    seller_id: 1,
-    buyer_id: 2,
-    product_id: "bell",
-    product_count: 1000000,
+    listingId: 1,
+    sellerId: 1,
+    buyerId: 2,
+    productId: 'bell',
+    productCount: 1000000,
   },
 ];
 
@@ -116,19 +116,19 @@ const usersDB = [
     id: 1,
     username: 'ppt',
     email: 'ppt@gmail.com',
-    switch_id: '1111-1111-1111',
-    island_name: 'ppt_island',
-    villager_list: ["B3RyfNEqwGmcccRC3"],
-    wishlist: ["QiRLGPEKXAy9CnsnD", "EFHuxMqC34e4se2EB", "dRTnqq9pYTtLGmZZt"],
+    switchId: '1111-1111-1111',
+    islandName: 'ppt_island',
+    villagerList: ['B3RyfNEqwGmcccRC3'],
+    wishlist: ['QiRLGPEKXAy9CnsnD', 'EFHuxMqC34e4se2EB', 'dRTnqq9pYTtLGmZZt'],
   },
   {
     id: 2,
     username: 'sfz',
     email: 'sfz@gmail.com',
-    switch_id: '2222-2222-2222',
-    island_name: 'sfz_island',
-    villager_list: ["SGMdki6dzpDZyXAw5"],
-    wishlist: ["dRTnqq9pYTtLGmZZt", "ihA8W7QC64yvcX4Dd", "hyZvWM6kXL5S7qcbN"],
+    switchId: '2222-2222-2222',
+    islandName: 'sfz_island',
+    villagerList: ['SGMdki6dzpDZyXAw5'],
+    wishlist: ['dRTnqq9pYTtLGmZZt', 'ihA8W7QC64yvcX4Dd', 'hyZvWM6kXL5S7qcbN'],
   },
 ];
 
@@ -136,26 +136,26 @@ db.listings.insertMany(listingsDB);
 db.offers.insertMany(offersDB);
 db.users.insertMany(usersDB);
 
-const listing_count = db.listings.count();
-const offer_count = db.offers.count();
-const user_count = db.users.count();
+const listingCount = db.listings.count();
+const offerCount = db.offers.count();
+const userCount = db.users.count();
 
-print('Inserted', listing_count, 'listings');
-print('Inserted', offer_count, 'offers');
-print('Inserted', user_count, 'users');
+print('Inserted', listingCount, 'listings');
+print('Inserted', offerCount, 'offers');
+print('Inserted', userCount, 'users');
 
 db.counters.remove({ _id: 'listings' });
 db.counters.remove({ _id: 'offers' });
 db.counters.remove({ _id: 'users' });
 
-db.counters.insert({ _id: 'listings', current: listing_count });
-db.counters.insert({ _id: 'offers', current: offer_count });
-db.counters.insert({ _id: 'users', current: user_count });
+db.counters.insert({ _id: 'listings', current: listingCount });
+db.counters.insert({ _id: 'offers', current: offerCount });
+db.counters.insert({ _id: 'users', current: userCount });
 
 db.listings.createIndex({ id: 1 }, { unique: true });
 db.listings.createIndex({ status: 1 });
-db.listings.createIndex({ seller_id: 1 });
-db.listings.createIndex({ product_id: 1 });
+db.listings.createIndex({ sellerId: 1 });
+db.listings.createIndex({ productId: 1 });
 db.listings.createIndex({ created: 1 });
 db.listings.createIndex({ expired: 1 });
 
@@ -163,30 +163,32 @@ db.deleted_listings.createIndex({ id: 1 }, { unique: true });
 
 db.offers.createIndex({ id: 1 }, { unique: true });
 db.offers.createIndex({ status: 1 });
-db.offers.createIndex({ listing_id: 1 });
-db.offers.createIndex({ seller_id: 1 });
-db.offers.createIndex({ buyer_id: 1 });
+db.offers.createIndex({ listingId: 1 });
+db.offers.createIndex({ sellerId: 1 });
+db.offers.createIndex({ buyerId: 1 });
 
 db.deleted_offers.createIndex({ id: 1 }, { unique: true });
 
 db.users.createIndex({ id: 1 }, { unique: true });
 db.users.createIndex({ username: 1 });
 db.users.createIndex({ email: 1 }, { unique: true });
-db.users.createIndex({ switch_id: 1 }, { unique: true });
-db.users.createIndex({ 'username': 'text', 'email': 'text', 'switch_id': 'text', 'island_name': 'text', });
+db.users.createIndex({ switchId: 1 }, { unique: true });
+db.users.createIndex({
+  username: 'text', email: 'text', switchId: 'text', islandName: 'text',
+});
 
 // use mongo import 4 json data file
 db.items.createIndex({ 'variants.uniqueEntryId': 1 }, { unique: true });
-db.items.createIndex({ 'sourceSheet': 1 });
-db.items.createIndex({ 'name': 'text' });
+db.items.createIndex({ sourceSheet: 1 });
+db.items.createIndex({ name: 'text' });
 
-db.villagers.createIndex({ 'uniqueEntryId': 1 }, { unique: true });
-db.villagers.createIndex({ 'personality': 1 });
-db.villagers.createIndex({ 'species': 1 });
-db.villagers.createIndex({ 'name': 'text' });
+db.villagers.createIndex({ uniqueEntryId: 1 }, { unique: true });
+db.villagers.createIndex({ personality: 1 });
+db.villagers.createIndex({ species: 1 });
+db.villagers.createIndex({ name: 'text' });
 
-db.recipes.createIndex({ 'uniqueEntryId': 1 }, { unique: true });
-db.recipes.createIndex({ 'name': 'text' });
+db.recipes.createIndex({ uniqueEntryId: 1 }, { unique: true });
+db.recipes.createIndex({ name: 'text' });
 
-db.nookmiles.createIndex({ 'uniqueEntryId': 1 }, { unique: true });
-db.nookmiles.createIndex({ 'name': 'text' });
+db.nookmiles.createIndex({ uniqueEntryId: 1 }, { unique: true });
+db.nookmiles.createIndex({ name: 'text' });
